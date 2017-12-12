0b4683def71a0e88ecdc09af9a5b826eba077e0c
Patrick Weber <pweber@spatialbit.com>
2017-12-12 14:39:18 +0100

    add download resources for dataset example

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-Simple example of how to update a file resource for a given dataset, using
-a specified API Key.
-
+This repository contains a list of examples of how to interact with the
+data.public.lu API to create, manage and download OpenData.
---
+++ download_resources.py
@@ -0,0 +1,29 @@
+import requests
+import argparse
+import json
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument('api_key', help="API key to use", type=str)
+parser.add_argument('dataset_id', type=str)
+args = parser.parse_args()
+
+api_key = args.api_key
+dataset_id = args.dataset_id
+
+headers = {}
+headers['X-API-KEY'] = api_key
+url = 'https://data.public.lu/api/1/datasets/%s/' % (dataset_id)
+r = requests.get(url, headers=headers)
+data = json.loads(r.text)
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+    return local_filename
+
+for resource in data['resources']:
+    print download_file(resource['url']) 
