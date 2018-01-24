9560f16a40f96612a3f4cea4692a997d67cd7f90
Patrick Weber <pweber@spatialbit.com>
2018-01-24 11:56:14 +0100

    added ability to set title on uploaded resource

--- upload_resource.py
+++ upload_resource.py
@@ -1,16 +1,19 @@
 import requests
+import json
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('api_key', help="API key to use", type=str)
 parser.add_argument('dataset_id', type=str)
 parser.add_argument('resource_id', type=str)
+parser.add_argument('resource_title', type=str)
 parser.add_argument('filename', help="file to upload")
 args = parser.parse_args()
 
 api_key = args.api_key
 dataset_id = args.dataset_id
 resource_id = args.resource_id
+resource_title = args.resource_title
 file_name = args.filename
 
 files = {'file': (args.filename, open(args.filename, 'rb'), 'text/plain')}
@@ -18,4 +21,10 @@ headers = {'X-API-KEY': api_key}
 url = 'https://data.public.lu/api/1/datasets/%s/resources/%s/upload/' % (dataset_id, resource_id)
 
 r = requests.post(url, files=files, headers=headers)
+resource = json.loads(r.text)
+
+resource['title'] = resource_title
+
+url = 'https://data.public.lu/api/1/datasets/%s/resources/%s/' % (dataset_id, resource_id)
+r = requests.put(url, json=resource, headers=headers)
 print r.text
